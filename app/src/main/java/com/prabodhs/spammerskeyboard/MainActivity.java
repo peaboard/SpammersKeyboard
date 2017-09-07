f03efed7ec21dd09bde465799cc1f024149ffdec
Prabodh-PC <Prabodh-PC>
2017-09-07 10:33:58 +0530

    Updated Main Activity Page and App Icon

--- app/src/main/java/com/prabodhs/spammerskeyboard/MainActivity.java
+++ app/src/main/java/com/prabodhs/spammerskeyboard/MainActivity.java
@@ -1,7 +1,13 @@
 package com.prabodhs.spammerskeyboard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Button implementation thanks
+            http://www.androidbegin.com/tutorial/android-button-click-new-activity-example/
+            Stack Overflow
+         */
+
+        // Locate the button in activity_main.xml
+        Button button1 = (Button)findViewById(R.id.ActivateKeyboard);
+        Button button2 = (Button)findViewById(R.id.KeyboardSettings);
+
+        // Capture button clicks
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                startActivityForResult(new Intent(android.provider.Settings.ACTION_INPUT_METHOD_SETTINGS), 0);
+
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(MainActivity.this, ImePreferences.class);
+                startActivity(i);
+
+            }
+        });
     }
+
 }
